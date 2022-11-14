29107a0d1594f587e7352eba22b1581d9031e98c
Leonel Escapita <leo.note0117@gmail.com>
2022-11-14 16:50:20 -0700

    Working secret, need to add cursor effect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,86 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hci-interface-hw';
+  mouseOverCondition = false;
+  mouseOutCondition = false;
+  buttonClicked = false;
+  shiftDown = false;
+  shiftUp = false;
+  sPressed = false;
+
+
+  onKeyDown(event: any){
+    this.shiftDown = true;
+  }
+
+  onKeyUp(event: any){
+    this.shiftUp = true;
+  }
+
+  onSKeyDown(event: any){
+    this.sPressed = true;
+  }
+
+  secretConditionsMet(){
+    console.log(this.mouseOverConditionMet(this.mouseOverCondition));
+    return this.mouseOverConditionMet(this.mouseOverCondition);
+  }
+
+  mouseOverConditionMet(mouseOverCondition: any){
+     if(mouseOverCondition){
+      return this.mouseHoverConditionMet(this.mouseOutCondition);
+     }
+     else{
+      return false;
+     }
+  }
+
+  mouseHoverConditionMet(mouseOutCondition: any){
+    if(mouseOutCondition){
+     return this.buttonClickedConditionMet(this.buttonClicked);
+    }
+    else{
+      return false;
+     }
+  }
+
+  buttonClickedConditionMet(buttonClicked: any){
+    if(buttonClicked){
+     return this.shiftDownConditionMet(this.shiftDown);
+    }
+    else{
+      return false;
+     }
+  }
+
+  shiftDownConditionMet(shiftDown: any){
+    if(shiftDown){
+      return this.shiftUpConditionMet(this.shiftUp);
+    }
+    else{
+      return false;
+     }
+  }
+
+  shiftUpConditionMet(shiftUp: any){
+    if(shiftUp){
+      return this.sPressedConditionMet(this.sPressed);
+    }
+    else{
+      return false;
+     }
+  }
+
+  sPressedConditionMet(sPressed: any){
+    if(sPressed){
+      return true;
+    }
+    else{
+      return false;
+     }
+  }
+  
 }
+
+
+
